3766e6f60f1a5db7cf048a5beffc1a1ab937b331
profsulaiman001-oss <profsulaiman001@gmail.com>
2026-04-04 22:54:10 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+// This file can be empty, but it must exist for the app to be installable!
+self.addEventListener('fetch', function(event) {
+  // Keeps the phone happy without complex caching
+});
